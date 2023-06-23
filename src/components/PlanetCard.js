0e871f9040702494701ec3abb4ab79a8cabeb6f5
Victor Schmoegel <victorschmoegel@gmail.com>
2023-06-23 17:37:36 -0300

    renderizando lista de planetas no componente SolarSystem

---
+++ src/components/PlanetCard.js
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+
+function PlanetCard({ planetName }, { planetImage }) {
+  return (
+    <div data-testid="planet-card">
+      <p>{ planetName }</p>
+      <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+    </div>
+  );
+}
+
+PlanetCard.propTypes = {
+  planetName: PropTypes.string.isRequired,
+  planetImage: PropTypes.string.isRequired,
+};
+
+export default PlanetCard;
--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,9 +1,17 @@
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 function SolarSystem() {
   return (
     <div data-testid="solar-system">
       <Title />
+      {planets.map((planet) => (<PlanetCard
+        key={ planet.name }
+        planetName={ planet.name }
+        planetImage={ planet.image }
+        />
+      ))}
     </div>
   );
 }
